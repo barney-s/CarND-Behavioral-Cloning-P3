4c97ce3574757c28768c0b3f1e7b9475c390d8e4
Barni S <bharanis78@gmail.com>
2017-04-18 21:27:14 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@
 #   but run data are under ~/Desktop/sim_data/run1 , ~/Desktop/sim_data/run2 etc
 # 
 
-python model.py --model comma.ai  --drive --datapath ~/Desktop/sim_data/
-#python model.py --model nvidia  --drive --datapath ~/Desktop/sim_data/
-
+python model.py --model comma.ai  --datapath ~/Desktop/sim_data/ --epochs 20  --drive
+# python model.py --model nvidia  --drive --datapath ~/Desktop/sim_data/
+# python model.py --model comma.ai   --datapath comma.ai/data/ --balanced --epochs 2
