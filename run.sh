4e92d41cc133cac84f93193c3777d5ca09a2921b
Barni S <bharanis78@gmail.com>
2017-04-19 13:08:38 -0700

    remove unused files

--- run.sh
+++
@@ -1,9 +0,0 @@
-# data path should have rundata under a subdir 
-# eg: 
-#   ~/Desktop/sim_data is tha parameter
-#   but run data are under ~/Desktop/sim_data/run1 , ~/Desktop/sim_data/run2 etc
-# 
-
-python model.py --model comma.ai  --datapath ~/Desktop/sim_data/ --epochs 20  --drive
-# python model.py --model nvidia  --drive --datapath ~/Desktop/sim_data/
-# python model.py --model comma.ai   --datapath comma.ai/data/ --balanced --epochs 2
